3fcc4060f4dd8c22306842b7ad17c14af3942f9c
Dennis Göries <43136580+dgoeries@users.noreply.github.com>
2023-05-17 13:57:22 +0200

    Align to newer pip and build system (#21)
    * Align to newer pip and build system
    
    * Change public testing

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ def get_script_path():
     return osp.dirname(osp.realpath(sys.argv[0]))
 
 
-def read(*parts):
-    return open(osp.join(get_script_path(), *parts)).read()
-
-
 class numpy_get_include:
     def __str__(self):
         import numpy
@@ -27,31 +23,5 @@ lttbc_py = Extension("lttbc", sources=["lttbc.c"],
                      )
 
 setup(
-    name="lttbc",
-    author="European XFEL GmbH",
-    use_scm_version=True,
     ext_modules=[lttbc_py],
-    author_email="dennis.goeries@xfel.eu",
-    maintainer="Dennis Goeries",
-    url="https://github.com/dgoeries/lttbc/",
-    description="Largest triangle three buckets module for Python written in C",
-    long_description=read("README.txt"),
-    long_description_content_type="text/markdown",
-    license="MIT",
-    install_requires=["numpy"],
-    setup_requires=["setuptools_scm"],
-    python_requires=">=3.5",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
 )
