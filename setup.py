a5ecc6bf1531c58af7974e42275cbc7750d2bb1b
Dennis Göries <43136580+dgoeries@users.noreply.github.com>
2024-10-23 03:16:52 +0200

    feat: Add numpy 2.X build and support for python 3.10+ (#27)
    * feat: Add numpy 2.X build and support for python 3.10+
    
    * Update git workflow

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-import os.path as osp
-import sys
-
 from setuptools import Extension, setup
-
-
-def get_script_path():
-    return osp.dirname(osp.realpath(sys.argv[0]))
 
 
 class numpy_get_include:
@@ -18,10 +11,8 @@ class numpy_get_include:
 lttbc_py = Extension("lttbc", sources=["lttbc.c"],
                      define_macros=[
                          ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-                     include_dirs=[numpy_get_include(),
-                                   get_script_path()],
+                     include_dirs=[numpy_get_include()],
                      )
-
 setup(
     ext_modules=[lttbc_py],
 )
